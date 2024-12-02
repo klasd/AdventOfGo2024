f48b11748f51116dd64ee4d40d8a68dd4fff3883
Klas Dahlén <6842922+klasd@users.noreply.github.com>
2024-12-02 21:55:01 +0100

    day2 part 1

--- main.go
+++ main.go
@@ -88,6 +88,67 @@ func similarityScore(left, right []int) int {
 	return weightedSum
 }
 
+func parseSeries(filePath string) (int, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, fmt.Errorf("Cant open file %w ", err)
+	}
+	defer file.Close()
+
+	var allLines [][]int
+	totalSafe := 0
+
+	fileReader := bufio.NewScanner(file)
+	for fileReader.Scan() {
+		line := fileReader.Text()
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+		numbers := strings.Fields(line)
+		var intNumbers []int
+		for _, strNum := range numbers {
+			num, err := strconv.Atoi(strNum)
+			if err != nil {
+				log.Fatalf("Error", err)
+			}
+			intNumbers = append(intNumbers, num)
+		}
+		allLines = append(allLines, intNumbers)
+	}
+	for _, line := range allLines {
+		if isInRange(line) {
+			totalSafe++
+		}
+	}
+	if err := fileReader.Err(); err != nil {
+		return 0, fmt.Errorf("syntax error: %w", err)
+	}
+
+	return totalSafe, nil
+}
+
+func isInRange(line []int) bool {
+	descending := true
+	if line[0] < line[1] {
+		descending = false
+	}
+	for i := 1; i < len(line); i++ {
+		diff := line[i] - line[i-1]
+		if diff == 0 {
+			return false
+		}
+		if descending == true && diff > 0 {
+			return false
+		}
+		if descending == false && diff < 0 {
+			return false
+		}
+		if abs(diff) > 3 {
+			return false
+		}
+	}
+	return true
+}
 func main() {
 	filePath := "day1/input"
 	left, right, err := parseInput(filePath)
@@ -100,4 +161,8 @@ func main() {
 
 	similarity := similarityScore(left, right)
 	fmt.Println(similarity)
+	filePath2 := "day2/input"
+
+	safe, _ := parseSeries(filePath2)
+	fmt.Println(safe)
 }
